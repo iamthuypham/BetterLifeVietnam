95ab14de9df64e66990a591c415370761fb839ad
iamthuypham <tpham045@hotmail.com>
2018-11-18 14:13:28 -0800

    First commit

---
+++ src/theme/MuiTheme.tsx
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+
+const theme: Theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#FF0000'
+    }
+  }
+});
+
+export default theme;
