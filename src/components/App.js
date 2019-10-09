ebfbec0d2c265fb1e0576050afdba08f87b778f0
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2019-10-09 15:14:05 -0500

    Initial commit: project directory structured to my liking and basic React Routes in place.

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Home from './Home.js';
+import About from './About.js';
+import Contact from './Contact.js';
+import Projects from './Projects.js';
+
+import '../css/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+            <Route exact path="/"  component={Home} />
+            <Route path="/about"  component={About} />
+            <Route path="/contact"  component={Contact} />
+            <Route path="/projects"  component={Projects} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
