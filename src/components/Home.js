2557fe21c7d010705ab994857bffd031ee79b96e
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2019-11-10 17:42:16 -0600

    Home image

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,21 @@ import React from 'react';
 import '../css/Home.css';
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div className="home page">
-                Home
-            </div>
-        );
-    }
+  windowInfo = () => {
+    console.log("window.innerWidth: ", window.innerWidth);
+    console.log("window.innerHeight: ", window.innerHeight);
+  }
+  render() {
+    return (
+      <div className="home-strip">
+        <div 
+              className="home-img"
+              // onClick={this.windowInfo}
+           >
+          </div>
+      </div>
+    )
+  }
 }
 
-export default Home; 
+export default Home;
