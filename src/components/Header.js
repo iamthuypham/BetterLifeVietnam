e618d9ac1b826e66a9ca9f9e0bb319b7391eaf5b
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2019-12-27 16:03:06 -0600

    react-material-ui reusable button component successfully implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,74 +1,87 @@
-import React from "react";
+import React from 'react'
 
-import "../css/Header.css";
+import '../css/Header.css'
 
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button'
 
 const MainHeader = () => {
   return (
-    <div className="header">
-      <div className="header-section">
-        <div className="section-half left">
-          <a href="/" className="logo"></a>
+    <div className='header'>
+      <div className='header-section'>
+        <div className='section-half left'>
+          <a href='/' className='logo'></a>
         </div>
-        <div className="section-half right">
-          <div className="social-media-links general">
+        <div className='section-half right'>
+          <div className='social-media-links general'>
             <a
-              href="https://facebook.com"
-              className="widget facebook"
-              target="_blank"
+              href='https://facebook.com'
+              className='widget facebook'
+              target='_blank'
             ></a>
             <a
-              href="https://twitter.com"
-              className="widget twitter"
-              target="_blank"
+              href='https://twitter.com'
+              className='widget twitter'
+              target='_blank'
             ></a>
             <a
-              href="https://youtube.com"
-              className="widget youtube"
-              target="_blank"
+              href='https://youtube.com'
+              className='widget youtube'
+              target='_blank'
             ></a>
             <a
-              href="https://instagram.com"
-              className="widget instagram"
-              target="_blank"
+              href='https://instagram.com'
+              className='widget instagram'
+              target='_blank'
             ></a>
           </div>
-          <div className="donate-button">Donate</div>
+          <div className='donate-button'>
+            <Button
+              style={{
+                height: '100%',
+                borderRadius: '10px',
+                backgroundColor: 'ff8e3a',
+                color: '#ffffff',
+                fontWeight: 'bold',
+                fontSize: '24px',
+                textTransform: 'none'
+              }}
+              fullWidth='true'
+              color='secondary' 
+              variant='contained'>
+              Donate
+            </Button>
+          </div>
         </div>
       </div>
-      <Button size="medium" color="secondary" variant="contained">
-        MU Button
-      </Button>
     </div>
-  );
-};
+  )
+}
 
 const MobileHeader = () => {
   return (
-    <div className="mobile-header">
-      <div className="mobile-header-section left">
-        <a href="/" className="mobile-logo"></a>
+    <div className='mobile-header'>
+      <div className='mobile-header-section left'>
+        <a href='/' className='mobile-logo'></a>
       </div>
-      <div className="mobile-header-section right">
-        <div className="burger-menu">
-          <div className="burger-strip"></div>
-          <div className="burger-strip"></div>
-          <div className="burger-strip"></div>
+      <div className='mobile-header-section right'>
+        <div className='burger-menu'>
+          <div className='burger-strip'></div>
+          <div className='burger-strip'></div>
+          <div className='burger-strip'></div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 class Header extends React.Component {
   state = {
     view: window.innerWidth > 400 ? <MainHeader /> : <MobileHeader />
-  };
-  render() {
-    const view = this.state.view;
-    return view;
+  }
+  render () {
+    const view = this.state.view
+    return view
   }
 }
 
-export default Header;
+export default Header
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Button from '@material-ui/core/Button'
+
 import '../css/Main.css'
 // okay so let's try something here...
 // going for a single strip -> Main
@@ -17,40 +19,49 @@ class Main extends React.Component {
     return (
       <div className='main-strip'>
         <div className='home-img'>
-          <div className="tagline-button-container">
-            <div className="tagline">Tag line</div>
-            <div className="learn-more">Learn More</div>
+          <div className='tagline-button-container'>
+            <div className='tagline'>Tag line</div>
+            <div className='learn-more'>
+              <Button
+                style={{
+                  height: '100%',
+                  borderRadius: '10px',
+                  backgroundColor: 'ff8e3a',
+                  color: '#ffffff',
+                  fontWeight: 'bold',
+                  fontSize: '24px',
+                  textTransform: 'none'
+                }}
+                fullWidth='true'
+                color='secondary'
+                variant='contained'
+              >
+                Learn More
+              </Button>
+            </div>
           </div>
         </div>
         <div className='section'>
           <div className='home-section-strip bold-style'>Our Mission</div>
-          <div className='home-section-strip education' >
-            <div className="quotation-mark">
-              "
-            </div>
-            Working to improve education conditions 
-            for underprivileged children and youth in Vietnam.
-            <div className="quotation-mark">
-              "
-            </div>
+          <div className='home-section-strip education'>
+            <div className='quotation-mark'>"</div>
+            Working to improve education conditions for underprivileged children
+            and youth in Vietnam.
+            <div className='quotation-mark'>"</div>
           </div>
-          <div className="home-section-strip"></div>
-          <div className='home-section-strip bold-style'>
-            Sponsors
-          </div>
+          <div className='home-section-strip'></div>
+          <div className='home-section-strip bold-style'>Sponsors</div>
           <div className='home-logos'>
-            <div className="home-logo hilton"></div>
-            <div className="home-logo hilton-opera"></div>
-            <div className="home-logo water-for-all"></div>
-            <div className="home-logo Viet-Dan-travel"></div>
+            <div className='home-logo hilton'></div>
+            <div className='home-logo hilton-opera'></div>
+            <div className='home-logo water-for-all'></div>
+            <div className='home-logo Viet-Dan-travel'></div>
           </div>
         </div>
-        <div className="section">
-          
-        </div>
+        <div className='section'></div>
       </div>
     )
   }
 }
 
-export default Main;
+export default Main
